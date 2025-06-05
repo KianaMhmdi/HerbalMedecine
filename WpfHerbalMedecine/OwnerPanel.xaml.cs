9889a7415799c67b429e17cbb812a90c098df7e3
Kiana <kiana.mohamadi2004@gmail.com>
2025-06-05 16:25:41 +0330

    - Creating the owner panel

---
+++ WpfHerbalMedecine/OwnerPanel.xaml.cs
@@ -0,0 +1,97 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace WpfHerbalMedecine
+{
+    /// <summary>
+    /// Interaction logic for OwnerPanel.xaml
+    /// </summary>
+    public partial class OwnerPanel : Window
+    {
+        public OwnerPanel()
+        {
+            InitializeComponent();
+        }
+
+        private void btnOwnersList_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void btnSellersList_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void btnSearchMedecines_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void btnRequestedMedecines_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void btnAddMedecines_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void btnDeleteMedecines_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void btnSearchCustomers_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void btnAddCustomers_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void btnDeleteCustomers_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void btnPurchaseReports_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void btnSalesReports_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void btnPersonalSettings_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void SellersListBack_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void LoginWindow_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+    }
+}
